b8086ab38029d995d7484b7b0f1273e06f91a1ec
Paul Griego <paulgriego029@gmail.com>
2022-03-15 12:54:48 -0600

    fixed styling

--- places.rb
+++ places.rb
@@ -1,8 +1,8 @@
 puts "3456"
 def places
-puts 3456/1000
-puts 3456%1000/100
-puts 3456%100/10
-puts 3456%10
+  puts 3456/1000
+  puts 3456%1000/100
+  puts 3456%100/10
+  puts 3456%10
 end
 puts places
