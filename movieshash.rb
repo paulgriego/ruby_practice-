0cd4555b957569efdc5fd9b3796e168a565e6280
Paul Griego <paulgriego029@gmail.com>
2022-03-15 13:15:02 -0600

    removed comm

--- movieshash.rb
+++ movieshash.rb
@@ -1,7 +1,7 @@
 movies = {
   "wolf of wall street" => 2013,
   "toy story" => 1995,
-  "inception" => 2010,
+  "inception" => 2010
 }
 movies.each { |movie,year| 
   puts "The movie #{movie} was released in #{year}!"
