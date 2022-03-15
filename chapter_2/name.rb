7282ea4bbf77357ae46e869c3d41f2cb0ddbe7c3
Paul Griego <paulgriego029@gmail.com>
2022-03-15 17:06:13 -0600

    adding new file

--- chapter_2/name.rb
+++ chapter_2/name.rb
@@ -1,2 +1,5 @@
-puts "Hello what is your name? "
-name = gets.chomp
+puts "Hello, what is your first name? "
+first_name= gets.chomp
+puts "And your last name?"
+last_name= gets.chomp
+puts "Cool! Your name is " + first_name + " " + last_name+"."
