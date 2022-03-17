2cc709dae12c360195cf1ccddf3192181de00156
Paul Griego <paulgriego029@gmail.com>
2022-03-16 18:33:06 -0600

    adding exercises

---
+++ chapter_4_loops_iterators/stop.rb
@@ -0,0 +1,10 @@
+#using a do loop
+loop do 
+  puts "Try and stop me!"
+  answer = gets.chomp
+
+  if answer == "STOP"   
+  break
+  end 
+end
+
---
+++ chapter_4_loops_iterators/stop2.rb
@@ -0,0 +1,6 @@
+stop = " "
+while stop != "Stop"
+  puts "I bet you can't stop me!"
+  stop=gets.chomp
+  puts "Try again!"
+end 
